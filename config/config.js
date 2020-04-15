8bde56de7f76705342d85915e3a327b711a20c69
prakash <prakash.selvaraj@outlook.com>
2020-04-15 21:47:08 +0530

    -- test script removed

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoUri: 'mongodb+srv://root:root123root@cluster0-ibc2e.mongodb.net/test'
+  mongoUri: 'mongodb://covid-19:587FZKVOnsia1BjkibcZJqXthZtOkpiAvjZU6ro3ymI58UpI7HLaclJsoU81IobbvjnAz2dTGTqHWCNFHxtNqw==@covid-19.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@covid-19@'
 }
